ea7af69587bb9c6b562dd831de8478ef3b739f48
lhoril <cristopherbarahona23@gmail.com>
2024-04-07 22:14:45 +0200

    ver 6

--- PilaDinamica/Program.cs
+++ PilaDinamica/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Pila<int> myPila = new Pila<int>(55);
+            Pila<int> myPila = new Pila<int>(5);
             myPila.Push(1);
             myPila.Push(2);
             myPila.Push(3);
@@ -14,8 +14,7 @@
             Console.WriteLine("Contains --> "+myPila.Contains(2));
             Console.WriteLine("Count --> " +myPila.Count);
             Console.WriteLine("Index --> " +myPila.IndexOf(1));
-            //myPila.RemoveAt(1);
-            myPila.Insert(2, 1);
+            myPila.Insert(3, 6);
             Console.WriteLine("Count Despres de Insert--> " + myPila.Count);
 
             foreach (int i in myPila)
@@ -24,7 +23,7 @@
             }
             Console.WriteLine();
             //myPila.Clear();
-            Console.WriteLine(myPila.Remove(2));
+            Console.WriteLine(myPila.Remove(1));
             foreach (int i in myPila)
             {
                 Console.Write(i + ", ");
