cdd15479d6f3f4ad084fe6a27440368fdf9a8f99
lhoril <cristopherbarahona23@gmail.com>
2024-04-08 13:26:59 +0200

    Ver FINAL

--- PilaDinamica/Pila.cs
+++ PilaDinamica/Pila.cs
@@ -13,7 +13,7 @@ namespace PilaDinamica
     public class Pila<T> : IEnumerable<T>, IEnumerable, IList<T>
     {
         private Node<T> top = null;
-        private int nElem = -1;
+        private int nElem = 0;
         #region Constructors
         public Pila()
         {
@@ -24,15 +24,17 @@ namespace PilaDinamica
         {
            Node<T> node = new Node<T>(info);
            this.top = node;
+            nElem++;
         }
         #endregion
+
         #region Propietats
         public bool Empty
         {
             get { return top == null; }
         }
 
-        public int Count => nElem+1;
+        public int Count => nElem;
 
         public bool IsReadOnly
         {
@@ -56,11 +58,18 @@ namespace PilaDinamica
                 target.Info = value;
             }
         }
+        #endregion
 
+        #region Metoths
+        /// <summary>
+        /// GoTo es una funcio que ens permet agafar una quantitat de numeros dins una caixa
+        /// </summary>
+        /// <param name="posicio"> index al que volem arribar</param>
+        /// <returns>Una variable de classe node amb els numeros desde la posicio que nosaltres volem</returns>
         private Node<T> GoTo(int posicio) // Metodo el cual te lleva a una posicion en la cual te mostrara la informacion actual i la de después
         {
             Node<T> caixa = top;
-            if(posicio >= 0 && posicio < this.Count)
+            if (posicio >= 0 && posicio < this.Count)
             {
                 for (int i = 0; i < posicio; i++)
                 {
@@ -75,8 +84,12 @@ namespace PilaDinamica
 
             return caixa;
         }
-        #endregion
-        #region Constructors
+        /// <summary>
+        /// Push es una funcio que crea una caixa que després la posara dins la pila
+        /// Al principi de tot canviant la posicio dels altres elements cap endevant 
+        /// deixant un espai pels nous elements
+        /// </summary>
+        /// <param name="info">info es la informació que volem posar dins la nostra pila en una caixa</param>
         public void Push(T info) // Mete un elemento delante tuyo
         {
             Node<T> node = new Node<T>(info);
@@ -91,7 +104,12 @@ namespace PilaDinamica
                 this.top = node;
             }
         }
-
+        /// <summary>
+        /// Pop es una funcio que elimina el primer element que tenim devant nostre (Top.info)
+        /// Baixant en 1 el numero de element en cas d'haver eliminat el numero.
+        /// </summary>
+        /// <returns>retorna el element que hem eliminat</returns>
+        /// <exception cref="Exception">Dona una excepcio en cas de que la pila sigui buida</exception>
         public T Pop() // Elimina el elemento que tienes delante tuyo
         {
             if (Empty) throw new Exception("STACK UNDERFLOW! STACK IS EMPTY!!!");
@@ -99,6 +117,7 @@ namespace PilaDinamica
             if(top.Seg == null)
             {
                 top = null;
+                nElem--;
             }
             else
             {
@@ -109,25 +128,33 @@ namespace PilaDinamica
             }
             return dada;
         }
-
+        /// <summary>
+        /// Contains es una funcio que permet cercar un element 
+        /// en una pila de elements. 
+        /// </summary>
+        /// <param name="info">Cerca un element dins la pila</param>
+        /// <returns>Retorna True or False depenent si lo ha trobat o no</returns>
+        /// <exception cref="ArgumentNullException">Donara Exception en cas de que el item que volem cerca sigui null</exception>
         public bool Contains(T info) // Busca un elemnto i retrurna True o false dependiendo si lo has esncontrado
         {
-            if (info == null) throw new Exception("No pot comparar amb valors nulls");
+            if (info == null) throw new ArgumentNullException("No pot comparar amb valors nulls");
             bool trobat = false;
-            int num = Convert.ToInt32(top.Info), num2 = Convert.ToInt32(info);
-            Node<T> cursor = top;
-            while (cursor != null && !trobat)
+            IEnumerator<T> cursor = this.GetEnumerator();
+            while (cursor.MoveNext() && !trobat)
             {
-                trobat = num.Equals(num2);
-                num = Convert.ToInt32(cursor.Info);
-                cursor = cursor.Seg;
+                trobat = cursor.Current.Equals(info);
             }
             return trobat;
         }
-
+        /// <summary>
+        /// IndexOf es una funcio que cercara un numero i després agafara el num de posicio dins una pila de elements
+        /// </summary>
+        /// <param name="item">item es el numero que cercarem</param>
+        /// <returns>Retornara el numero de popsicio o -1 depenent si s'ha trobat el numero o no</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public int IndexOf(T item) // Busca un elemento si trobat es igual a true devolvera el numero de posicion de elemento y si es false devolvera -1
         {
-            if (item == null) throw new Exception("No pot comparar amb valors nulls");
+            if (item == null) throw new ArgumentNullException("No pot comparar amb valors nulls");
             bool trobat = false;
             int index = -1;
             IEnumerator<T> cursor = this.GetEnumerator();
@@ -139,11 +166,17 @@ namespace PilaDinamica
             if(!trobat) index = -1;
             return index;
         }
-
+        /// <summary>
+        /// Insertara un numero dins la posico que demanem
+        /// </summary>
+        /// <param name="index">Numero de posicio dins la pila</param>
+        /// <param name="item">Numero que voldrem insertar dins la pila</param>
+        /// <exception cref="ArgumentOutOfRangeException">Donara exception en cas de que index estigui fora del rang de elements de la pila</exception>
+        /// <exception cref="NotSupportedException">Donara exception en cas de que la pila sigui de només lectura</exception>
         public void Insert(int index, T item) // Inserta una informacion a partir de una informacion
         {
-            if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException("index");
-            if (IsReadOnly) throw new NotSupportedException();
+            if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException("El index no esta dins el rang de elements");
+            if (IsReadOnly) throw new NotSupportedException("No es pot editar la pila es només de lectura");
             Node<T> itNum = new Node<T>(item);
             if (index == 0)
                 Add(item);
@@ -157,17 +190,23 @@ namespace PilaDinamica
             }
             else
             {
-                Node<T> antCaixa = GoTo(index - 1);
+                Node<T> antCaixa = GoTo(index-1);
                 Node<T> ultCaixa = antCaixa.Seg; // Caja que recuperara los ultimos enlaces
                 antCaixa.Seg = itNum;
                 itNum.Seg = ultCaixa;
                 nElem++;
             }
         }
+        /// <summary>
+        /// Elimina un numero per posicio
+        /// </summary>
+        /// <param name="index">posicio dek item que voldrem eliminar</param>
+        /// <exception cref="ArgumentOutOfRangeException">Donara exception en cas de que el index sigui major o menor a la cuantitat de elements</exception>
+        /// <exception cref="NotSupportedException">Donara exception en cas de que la pila sigui de només lectura</exception>
         public void RemoveAt(int index) // RemoveAt elimina un item por index o posicion
         {
-            if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException("index");
-            if (IsReadOnly) throw new NotSupportedException();
+            if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException("El index no esta dins el rang de elements");
+            if (IsReadOnly) throw new NotSupportedException("No es pot editar la pila es només de lectura");
             if (index == 0)
             {
                 Pop();
@@ -191,16 +230,25 @@ namespace PilaDinamica
                 nElem--;
             }
         }
-
+        /// <summary>
+        /// Elimina un item concret
+        /// </summary>
+        /// <param name="item">El numero que volem eliminar</param>
+        /// <returns>Si s'ha eliminat el numero retorna True i si no False</returns>
+        /// <exception cref="NotSupportedException">Donara exception si la pila es només de lectura</exception>
         public bool Remove(T item) // Remove elimina un item
         {
-            bool trobat = false;
+            if (IsReadOnly) throw new NotSupportedException("No es pot editar la pila es només de lectura");
+            bool isRemoved = false;
             int index = IndexOf(item);
             RemoveAt(index);
-            if(!Contains(item)) trobat = true;
-            return trobat;
+            if(!Contains(item)) isRemoved = true;
+            return isRemoved;
         }
-
+        /// <summary>
+        /// Elimina tots els elements de la pila
+        /// </summary>
+        /// <exception cref="NotSupportedException"> Donara exception en cas de que la pila sigui només de lectura</exception>
         public void Clear() // Elimina todos los elementos
         {
             if (IsReadOnly) throw new NotSupportedException("No es pot eliminar cap cosa de la pila es només de lectura");
@@ -209,22 +257,37 @@ namespace PilaDinamica
                 Pop();
             }
         }
-
+        /// <summary>
+        /// Copia una informacio d'un array a partir d'una posicio i la coloca amb un push a la pila
+        /// </summary>
+        /// <param name="array">L'array amb la informacio a copiar</param>
+        /// <param name="arrayIndex">El numero de elements per posicio</param>
+        /// <exception cref="ArgumentNullException">Donara exception en cas de que l'array sigui null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Donara exception en cas de que el numero de posicio sigui menor a la capacitat del array</exception>
+        /// <exception cref="ArgumentException">Donara Exception en cas de que el index sigui major a la capacitat del array</exception>
         public void CopyTo(T[] array, int arrayIndex) // Copia una informacion de un array a partir de un index i la coloca con un push a la pila
         {
+            if(array == null) throw new ArgumentNullException("El contingut del array no pot ser null");
+            if (arrayIndex < 0) throw new ArgumentOutOfRangeException("El index no pot ser menor a la capacitat del array");
+            if (arrayIndex > array.Length) throw new ArgumentException("El index no pot ser mayor a la capacitat del array");
             for (int i = 0; i < arrayIndex; i++)
             {
                 Add(array[i]);
             }
         }
-
+        /// <summary>
+        /// Afegeix numeros a la primera posicio
+        /// </summary>
+        /// <param name="item">es el numero que volem afegir</param>
+        /// <exception cref="NotSupportedException">Donara exception si la pila es de només lectura</exception>
         public void Add(T item) // Añades un numero
         {
-            if (item == null) throw new ArgumentNullException("ITEM NO POT SER NULL");
+            if (IsReadOnly) throw new NotSupportedException("No es pot afegir cap element a una pila de només lectura");
             Push(item);
         }
 
         #endregion
+
         #region Interficies
 
         public IEnumerator<T> GetEnumerator()
--- PilaDinamica/Program.cs
+++ PilaDinamica/Program.cs
@@ -9,26 +9,40 @@
             myPila.Push(2);
             myPila.Push(3);
             myPila.Push(4);
-            //myPila.Pop();
-            Console.WriteLine("indexOf --> "+myPila.IndexOf(55));
-            Console.WriteLine("Contains --> "+myPila.Contains(2));
-            Console.WriteLine("Count --> " +myPila.Count);
-            Console.WriteLine("Index --> " +myPila.IndexOf(1));
-            myPila.Insert(0, 6);
-            Console.WriteLine("Count Despres de Insert--> " + myPila.Count);
-
+            Console.Write("[ ");
             foreach (int i in myPila)
             {
-                Console.Write(i +", ");
+                Console.Write(i + " ");
             }
+            Console.Write("]");
             Console.WriteLine();
-            //myPila.Clear();
-            Console.WriteLine(myPila.Remove(2));
+            Console.WriteLine("Count despés de Pop --> " + myPila.Count);
+            myPila.Pop();
+            Console.WriteLine("indexOf --> "+myPila.IndexOf(5));
+            Console.WriteLine("Contains --> "+myPila.Contains(2));
+            Console.WriteLine("Contains --> "+myPila.Contains(7));
+            Console.WriteLine("Count --> " +myPila.Count);
+            Console.WriteLine("Index --> " +myPila.IndexOf(7));
+            myPila.Insert(3, 6);
+            Console.WriteLine("Count Despres de Insert --> " + myPila.Count);
+            Console.Write("[ ");
             foreach (int i in myPila)
             {
-                Console.Write(i + ", ");
+                Console.Write(i +" ");
             }
-            Console.WriteLine("Count --> " + myPila.Count);
+            Console.Write("]");
+            Console.WriteLine();
+            Console.WriteLine(myPila.Remove(2));
+            Console.Write("[ ");
+            foreach (int i in myPila)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+            Console.WriteLine("Count després del Remove --> " + myPila.Count);
+            myPila.Clear();
+            Console.WriteLine("Count despés del Clear --> " + myPila.Count);
         }
     }
 }
